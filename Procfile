5b87238dc8138f3ef46b61fc3e7a4445285e4d6d
saugat <saugatshrestha602@gmail.com>
2022-05-15 22:40:58 +0545

    make it better

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: gunicorn mysite.wsgi --log-file -
-celery -A mysite.celery worker --pool=solo -l info
--- mysite/settings.py
+++ mysite/settings.py
@@ -84,6 +84,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 ROOT_URLCONF = 'mysite.urls'
 
 TEMPLATES = [
